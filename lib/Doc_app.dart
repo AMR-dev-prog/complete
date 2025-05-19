bd365eab2eb88116a9a672ae849524c1c357057b
AMR-dev-prog <araed2178@gmail.com>
2025-05-19 17:57:38 +0300

    Revert "onbording 2"
    This reverts commit 7ced3cf619a9eaeaa34b52ccc2f19d60dbf0ef28.

--- lib/Doc_app.dart
+++ lib/Doc_app.dart
@@ -1,32 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/core/routing/app_router.dart';
-import 'package:flutter_application_1/core/routing/routes.dart';
-import 'package:flutter_application_1/core/theming/colors.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-
-
 
 class DocApp extends StatelessWidget {
-final AppRouter appRouter ;
-  const DocApp({super.key, required this.appRouter});
+  const DocApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(375, 812),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      builder: (context, child) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'Doc App',
-          theme: ThemeData(
-            primaryColor: ColorsManger.mainBlue,
-          ),
-          onGenerateRoute: appRouter.generateRoute,
-          initialRoute: Routes.onBording,
-        );
-      },
-    );
+    return const Placeholder();
   }
 }
